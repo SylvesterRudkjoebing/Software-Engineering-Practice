f77513593bd8a067e001089703a429bd5b342cd1
Sylvester Rudkjøbing <sylvrud@gmail.com>
2023-09-25 22:30:54 +0200

    Added print of data summary

--- Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityCSVProcessor.java
+++ Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityCSVProcessor.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,7 +31,7 @@ public class CityCSVProcessor {
 				String city = convertToString(rawValues[2]);
 				int population = convertToInt(rawValues[3]);
 				
-				System.out.println("id: " + id + ", year: " + year + ", city: " + city + ", population: " + population);
+				//System.out.println("id: " + id + ", year: " + year + ", city: " + city + ", population: " + population);
 				
 				//Creates object cityRecord with the current line and adds it to a list of cityRecords
 				CityRecord cityRecord = new CityRecord(id, year, city, population);
@@ -79,5 +80,41 @@ public class CityCSVProcessor {
 		
 		reader.readAndProcess(csvFile);
 
+		reader.printAllRecords();
+
+		reader.printRecordsByCity();
 	}
+
+	public void printRecordsByCity() {
+        for (Map.Entry<String, List<CityRecord>> entry : recordsByCity.entrySet()) {
+            String cityName = entry.getKey();
+            List<CityRecord> cityRecords = entry.getValue();
+
+			List<Integer> years = new ArrayList<>();
+			List<Integer> populations = new ArrayList<>();
+			int entries = cityRecords.size();
+
+            for (CityRecord record : cityRecords) {
+                years.add(record.year());
+				populations.add(record.population());
+            }
+
+			// Finds Average Population in the Entries
+			int sum = 0;
+			for (Integer i : populations) {
+				sum += i;
+			}
+
+			float avg = sum/entries;
+
+			// Sorts list to find minimum and maximum year
+			int min = Collections.min(years);
+			int max = Collections.max(years);
+
+			// Prints the data aggregations
+			String formattedString = String.format("%s (%d-%d; %d entries): %.0f", cityName, min, max, entries, avg);
+			System.out.println(formattedString);
+
+        }
+    }
 }
