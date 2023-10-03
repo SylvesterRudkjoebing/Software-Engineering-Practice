490c23f1435b16e4c2c556283d85a9d776d08d24
Sylvester Rudkjøbing <sylvrud@gmail.com>
2023-10-03 02:08:15 +0200

    Homework done

--- Project 1/Phase 1/Code/Phase 1/src/org/nypl/journalsystem/Article.java
+++ Project 1/Phase 1/Code/Phase 1/src/org/nypl/journalsystem/Article.java
@@ -2,5 +2,5 @@ package org.nypl.journalsystem;
 
 import java.util.List;
 
-public record Article(int id, String title, List<Integer> authors, String issn) {
+public record Article(int id, String title, List<Integer> authorids, String issn) {
 }
--- Project 1/Phase 1/Code/Phase 1/src/org/nypl/journalsystem/LibrarySystem.java
+++ Project 1/Phase 1/Code/Phase 1/src/org/nypl/journalsystem/LibrarySystem.java
@@ -1,5 +1,6 @@
 package org.nypl.journalsystem;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,12 +8,20 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
 public class LibrarySystem {
 	private List<Journal> journals;
+	private List<Author> authors;
+	private List<Article> articles;
 	
 	public LibrarySystem() {
         // Initialize the list of journals
         journals = new ArrayList<>();
+		authors = new ArrayList<>();
+		articles = new ArrayList<>();
 
         // Create and add default journals
         Publisher springer = new Publisher("Springer", "Germany");
@@ -43,26 +52,124 @@ public class LibrarySystem {
 	}
 	
 	protected void loadAuthors() throws FileNotFoundException, IOException {
-		File file = new File("data/Authors.csv");
+		File file = new File("Project 1/Phase 1/Code/Phase 1/data/Authors.csv");
+	
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			// Skip the header row
+			reader.readLine();
+	
+			CSVParser csvParser = CSVFormat.DEFAULT.parse(reader);
+	
+			for (CSVRecord csvRecord : csvParser) {
+				int id = Integer.parseInt(csvRecord.get(0)); // Assuming ID is in the first column (index 0)
+				String authorName = csvRecord.get(1); // Assuming Name is in the second column (index 1)
+				String authorSurname = csvRecord.get(2);
 
-		//TODO: Load authors from file
+				String fullname = authorSurname + authorName;
+				String nameWithoutQuotes = fullname.replace("\"", "");
+
+	
+				// Create an Author object and add it to your system
+				Author author = new Author(id, nameWithoutQuotes);
+				// Add the author to your system's data structure (e.g., a list of authors)
+				authors.add(author);
+			}
+		}
 	}
 	
 	protected void loadArticles() throws FileNotFoundException, IOException {
-		File file = new File("data/Articles.csv");
+		File file = new File("Project 1/Phase 1/Code/Phase 1/data/Articles.csv");
 
-		//TODO: Load articles from file and assign them to appropriate journal
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			String line;
+			boolean isFirstLine = true; // Flag to skip the first line (header row)
+			while ((line = reader.readLine()) != null) {
+				if (isFirstLine) {
+					isFirstLine = false;
+					continue; // Skip the header row
+				}
+				// Process each line of the CSV file to extract article information
+				String[] articleData = line.split(",");
+				// Create and add Article objects to your system as needed
+				if (articleData.length == 4) {
+					int id = Integer.parseInt(articleData[0]);
+					String title = articleData[1];
+					List<Integer> authorIds = parseAuthorIds(articleData[2]);
+					String issn = articleData[3];
+					
+					// Create an Article object and add it to your system
+					Article article = new Article(id, title, authorIds, issn);
+					// Add the article to your system's data structure (e.g., a list of articles)
+					articles.add(article);
+
+					for (Journal journal : journals) {
+						if (journal.issn().equals(issn.trim())) {
+							journal.articles().add(article);
+						}
+					}
+				}
+			}
+		}
 	}
 	
+	private List<Integer> parseAuthorIds(String authorIdsString) {
+		List<Integer> authorIds = new ArrayList<>();
+		// Remove square brackets and split by semicolons
+		String[] parts = authorIdsString.replace("[", "").replace("]", "").split(";");
+		
+		for (String part : parts) {
+			authorIds.add(Integer.parseInt(part.trim()));
+		}
+		
+		return authorIds;
+	}
 	
 	public void listContents() {
-		//TODO: Print all journals with their respective articles and authors to the console.
+		for (Journal journal : journals) {
+			System.out.println("Journal Name: " + journal.name());
+			System.out.println("Publisher: " + journal.publisher().name());
+			System.out.println("ISSN: " + journal.issn());
+	
+			if (journal.isFull()) {
+				System.out.println("Full Issue: Yes");
+			} else {
+				System.out.println("Full Issue: No");
+			}
+	
+			System.out.println("Articles:");
+	
+			for (Article article : journal.articles()) {
+				System.out.println("  Article Title:" + article.title());
+	
+				System.out.print("  Authors:");
+				for (Integer authorId : article.authorids()) {
+					// Assuming you have a list of authors with their IDs
+					String authorname = findAuthorNameById(authorId);
+					System.out.print(authorname + ",");
+
+
+				}
+				System.out.println();
+			}
+	
+			System.out.println();
+		}
 	}
 	
+	public String findAuthorNameById(int authorId) {
+		for (Author author : authors) {
+			if (author.id() == authorId) {
+				return author.name();
+			}
+		}
+		return "Unknown"; // Author not found
+	}
+
 	public static final void main(String[] args) throws Exception {
 		LibrarySystem librarySystem = new LibrarySystem();
 		
 		librarySystem.load();
+		
 		librarySystem.listContents();
 	}
 }
